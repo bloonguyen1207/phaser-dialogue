d87c2b0b4153cbcd473be713be25986f556cc01e
Bloo Nguyen <bloo.nguyen@airthings.com>
2023-02-11 22:37:58 +0100

    empty lines at eof

--- custom.d.ts
+++ custom.d.ts
@@ -1,5 +1,13 @@
+// Images
 declare module '*.png';
 declare module '*.svg';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.gif';
+
+// Fonts
+declare module '*.woff';
+declare module '*.woff2';
+declare module '*.eot';
+declare module '*.ttf';
+declare module '*.otf';
