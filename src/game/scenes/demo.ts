8a497d4a4fea4af9abc2216e3a9ce1a98889c667
Bloo Nguyen <bloo.nguyen@airthings.com>
2023-08-20 11:57:59 +0200

    lint

--- src/game/scenes/demo.ts
+++ src/game/scenes/demo.ts
@@ -1,17 +1,16 @@
-import textbox from '../../assets/images/textbox1.png';
+import textbox from '../../assets/images/textbox1.png'
 
 export default class Demo extends Phaser.Scene {
-    constructor ()
-    {
-        super('demo');
-    }
+  constructor () {
+    super('demo')
+  }
 
-    preload () {
-        this.load.image("test_textbox", textbox)
-    }
+  preload (): void {
+    this.load.image('test_textbox', textbox)
+  }
 
-    create () {
-        this.add.text(50, 50, "Hello world!")
-        this.add.image(100, 200, "test_textbox")
-    }
+  create (): void {
+    this.add.text(50, 50, 'Hello world!')
+    this.add.image(100, 200, 'test_textbox')
+  }
 }
--- src/plugin/main.ts
+++ src/plugin/main.ts
@@ -1,77 +1,86 @@
-import { Plugins, Scene, Scenes } from "phaser";
+import { type Plugins, type Scene, type Scenes } from 'phaser'
 
 const register = (PluginManager: Plugins.PluginManager): void => {
-    PluginManager.installScenePlugin('DialoguePlugin', DialoguePlugin, 'dialogue')
+  PluginManager.installScenePlugin('DialoguePlugin', DialoguePlugin, 'dialogue')
 }
 
 class DialoguePlugin {
-    scene: Scene | undefined
-    systems: Scenes.Systems
+  scene: Scene | undefined
+  systems: Scenes.Systems
 
-    constructor(scene: Scene) {
-        //  The Scene that owns this plugin
-        this.scene = scene;
-        this.systems = scene.sys
+  constructor (scene: Scene) {
+    //  The Scene that owns this plugin
+    this.scene = scene
+    this.systems = scene.sys
 
-        if (!scene.sys.settings.isBooted) scene.sys.events.once('boot', this.boot, this);
-    }
+    if (!scene.sys.settings.isBooted) scene.sys.events.once('boot', this.boot, this)
+  }
 
-    //  Called when the Plugin is booted by the PluginManager.
-    //  If you need to reference other systems in the Scene (like the Loader or DisplayList) then set-up those references now, not in the constructor.
-    boot() {
-        const eventEmitter = this.systems.events;
+  //  Called when the Plugin is booted by the PluginManager.
+  //  If you need to reference other systems in the Scene (like the Loader or DisplayList) then set-up those references now, not in the constructor.
+  boot (): void {
+    const eventEmitter = this.systems.events
 
-        //  Listening to the following events is entirely optional, although we would recommend cleanly shutting down and destroying at least.
-        //  If you don't need any of these events then remove the listeners and the relevant methods too.
+    //  Listening to the following events is entirely optional, although we would recommend cleanly shutting down and destroying at least.
+    //  If you don't need any of these events then remove the listeners and the relevant methods too.
 
-        eventEmitter.on('start', this.start, this);
+    eventEmitter.on('start', this.start, this)
 
-        eventEmitter.on('preupdate', this.preUpdate, this);
-        eventEmitter.on('update', this.update, this);
-        eventEmitter.on('postupdate', this.postUpdate, this);
+    eventEmitter.on('preupdate', this.preUpdate, this)
+    eventEmitter.on('update', this.update, this)
+    eventEmitter.on('postupdate', this.postUpdate, this)
 
-        eventEmitter.on('pause', this.pause, this);
-        eventEmitter.on('resume', this.resume, this);
+    eventEmitter.on('pause', this.pause, this)
+    eventEmitter.on('resume', this.resume, this)
 
-        eventEmitter.on('sleep', this.sleep, this);
-        eventEmitter.on('wake', this.wake, this);
+    eventEmitter.on('sleep', this.sleep, this)
+    eventEmitter.on('wake', this.wake, this)
 
-        eventEmitter.on('shutdown', this.shutdown, this);
-        eventEmitter.on('destroy', this.destroy, this);
-    }
+    eventEmitter.on('shutdown', this.shutdown, this)
+    eventEmitter.on('destroy', this.destroy, this)
+  }
 
-    //  Called when a Scene is started by the SceneManager. The Scene is now active, visible and running.
-    start() {}
+  //  Called when a Scene is started by the SceneManager. The Scene is now active, visible and running.
+  start (): void {
+  }
 
-     //  Called every Scene step - phase 1
-    preUpdate(_time: any, _delta: any) {}
+  //  Called every Scene step - phase 1
+  preUpdate (_time: any, _delta: any): void {
+  }
 
-    //  Called every Scene step - phase 2
-    update(_time: any, _delta: any) {}
+  //  Called every Scene step - phase 2
+  update (_time: any, _delta: any): void {
+  }
 
-    //  Called every Scene step - phase 3
-    postUpdate(_time: any, _delta: any) {}
+  //  Called every Scene step - phase 3
+  postUpdate (_time: any, _delta: any): void {
+  }
 
-    //  Called when a Scene is paused. A paused scene doesn't have its Step run, but still renders.
-    pause() {}
+  //  Called when a Scene is paused. A paused scene doesn't have its Step run, but still renders.
+  pause (): void {
+  }
 
-    //  Called when a Scene is resumed from a paused state.
-    resume() {}
+  //  Called when a Scene is resumed from a paused state.
+  resume (): void {
+  }
 
-    //  Called when a Scene is put to sleep. A sleeping scene doesn't update or render, but isn't destroyed or shutdown. preUpdate events still fire.
-    sleep() {}
+  //  Called when a Scene is put to sleep. A sleeping scene doesn't update or render, but isn't destroyed or shutdown. preUpdate events still fire.
+  sleep (): void {
+  }
 
-    //  Called when a Scene is woken from a sleeping state.
-    wake() {}
+  //  Called when a Scene is woken from a sleeping state.
+  wake (): void {
+  }
 
-    //  Called when a Scene shuts down, it may then come back again later (which will invoke the 'start' event) but should be considered dormant.
-    shutdown() {}
+  //  Called when a Scene shuts down, it may then come back again later (which will invoke the 'start' event) but should be considered dormant.
+  shutdown (): void {
+  }
 
-    //  Called when a Scene is destroyed by the Scene Manager. There is no coming back from a destroyed Scene, so clear up all resources here.
-    destroy() {
-        this.shutdown();
-        this.scene = undefined;
-    }
+  //  Called when a Scene is destroyed by the Scene Manager. There is no coming back from a destroyed Scene, so clear up all resources here.
+  destroy (): void {
+    this.shutdown()
+    this.scene = undefined
+  }
 }
 
 export default DialoguePlugin
